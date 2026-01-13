9cb87b4a2d166b927fd9c40b09f04b8892d2c2fa
rahmanbokshi66-cloud <rahmanbokshi66@gmail.com>
2026-01-13 18:02:42 +0600

    code added

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* ডাইনামিক রাউটিং এর জন্য নিচের লাইনটি সরিয়ে দিন বা কমেন্ট করুন */
+  // output: 'export', 
 };
 
 export default nextConfig;
